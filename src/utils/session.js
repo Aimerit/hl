4ec94bc8614d8da88ea2a83ed06d57c076c0f80e
thetechshrine <thetechshrine@gmail.com>
2021-05-19 22:58:15 +0200

    setup cookies manually

--- src/utils/session.js
+++ src/utils/session.js
@@ -1,5 +1,6 @@
 import { AUTHENTICATED_KEY, TOKEN_KEY, SESSION_ID_KEY } from '../config/constants';
 import { isNullish } from './data_validation';
+import cookiesUtils from './cookies';
 
 function isAuthenticated() {
   return getLocalStorageValue(AUTHENTICATED_KEY) && !isNullish(getLocalStorageValue(TOKEN_KEY)) && !isNullish(getLocalStorageValue(SESSION_ID_KEY));
@@ -13,12 +14,16 @@ function initSession({ token, session } = {}) {
   localStorage.setItem(AUTHENTICATED_KEY, true);
   localStorage.setItem(TOKEN_KEY, token);
   localStorage.setItem(SESSION_ID_KEY, session.id);
+  cookiesUtils.setCookie(TOKEN_KEY, token);
+  cookiesUtils.setCookie(SESSION_ID_KEY, session.id);
 }
 
 function closeSession() {
   localStorage.removeItem(AUTHENTICATED_KEY);
   localStorage.removeItem(TOKEN_KEY);
   localStorage.removeItem(SESSION_ID_KEY);
+  cookiesUtils.removeCookie(TOKEN_KEY);
+  cookiesUtils.removeCookie(SESSION_ID_KEY);
 }
 
 export default { isAuthenticated, initSession, closeSession };
